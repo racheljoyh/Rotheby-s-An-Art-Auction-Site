cdb88d19b62f3f6300b0a33272357ef763856886
Rachel <racheljhumes@gmail.com>
2023-01-05 22:08:05 -0700

    login logout functionality tweaked

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -1,94 +1,86 @@
-import './App.css'
-import { useState, useEffect} from "react"
-import { Route, Switch, useHistory} from "react-router-dom"
-import HomePage from "/src/components/HomePage"
-import NavBar from "/src/components/NavBar"
-import BuyerProfile from "/src/components/BuyerProfile"
-import Artworks from "/src/components/Artworks"
-import ArtworkDetails from './components/ArtworkDetails'
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import HomePage from "/src/components/HomePage";
+import NavBar from "/src/components/NavBar";
+import BuyerProfile from "/src/components/BuyerProfile";
+import Artworks from "/src/components/Artworks";
+import ArtworkDetails from "./components/ArtworkDetails";
 
 function App() {
-
-  const [allArtwork, setAllArtwork]= useState([])
+  const [allArtwork, setAllArtwork] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  let [currentBuyer, setCurrentBuyer] = useState('');
+  let [currentBuyer, setCurrentBuyer] = useState("");
   const [allBuyers, setAllBuyers] = useState([]);
 
   // Fetching all Artwork
   useEffect(() => {
     fetch("http://localhost:9292/artworks")
-    .then((res) => res.json())
-    .then((artwork) => setAllArtwork(artwork));
-  }, [])
+      .then((res) => res.json())
+      .then((artwork) => setAllArtwork(artwork));
+  }, []);
 
   // console.log(allArtwork)
-  let history = useHistory()
+  let history = useHistory();
 
-
-  function handleLoginClick(){
-    setIsLoggedIn((isLoggedIn) => !isLoggedIn)
-    history.push('/Artworks')
+  function handleLoginClick() {
+    setIsLoggedIn((isLoggedIn) => !isLoggedIn);
+    !isLoggedIn ? history.push("/Artworks") : history.push("/");
   }
 
-// GETing buyer profiles
+  // GETing buyer profiles
   useEffect(() => {
     fetch("http://localhost:9292/buyers")
-    .then((res) => res.json())
-    .then(setAllBuyers);
+      .then((res) => res.json())
+      .then(setAllBuyers);
   }, []);
 
   // finding current buyer
-  function handleChangeBuyers(e){
+  function handleChangeBuyers(e) {
     // let fullName = `${buyer.first_name} + ${buyer.last_name}`
-    currentBuyer = allBuyers.find((buyer) => buyer.first_name === e.target.value);
+    currentBuyer = allBuyers.find(
+      (buyer) => buyer.first_name === e.target.value
+    );
     setCurrentBuyer(currentBuyer);
   }
 
   // console.log(currentBuyer)
 
-    // function filteredArt(selectedArt){
-    //     const clickedArt = artwork.filter((art) => art.id === selectedArt.id);
-    //     setArtwork(clickedArt)
-    // }
+  // function filteredArt(selectedArt){
+  //     const clickedArt = artwork.filter((art) => art.id === selectedArt.id);
+  //     setArtwork(clickedArt)
+  // }
 
   return (
     <div>
-        <NavBar
-          isLoggedIn={isLoggedIn}
-          currentBuyer={currentBuyer}
-        />
-        <Switch>
-            <Route exact path = "/">
-              <HomePage
-                onLoginClick={handleLoginClick}
-                currentBuyer={currentBuyer}
-                setCurrentBuyer={setCurrentBuyer}
-                onChangeBuyers={handleChangeBuyers}
-                setAllBuyers={setAllBuyers}
-                allBuyers={allBuyers}
-              />
-            </Route>
-            <Route exact path = "/Artworks" >
-              <Artworks
-                allArtwork={allArtwork}
-                currentBuyer={currentBuyer}
-              />
-            </Route>
-            <Route exact path = "/myProfile">
-              <BuyerProfile
-                currentBuyer={currentBuyer}
-                setCurrentBuyer={setCurrentBuyer}
-              />
-            </Route>
-            <Route path = "/artworks/:id">
-              <ArtworkDetails
-                currentBuyer={currentBuyer}
-              />
-            </Route>
-        </Switch>
+      <NavBar isLoggedIn={isLoggedIn} currentBuyer={currentBuyer} />
+      <Switch>
+        <Route exact path="/">
+          <HomePage
+            onLoginClick={handleLoginClick}
+            currentBuyer={currentBuyer}
+            setCurrentBuyer={setCurrentBuyer}
+            onChangeBuyers={handleChangeBuyers}
+            setAllBuyers={setAllBuyers}
+            allBuyers={allBuyers}
+            isLoggedIn={isLoggedIn}
+          />
+        </Route>
+        <Route exact path="/Artworks">
+          <Artworks allArtwork={allArtwork} currentBuyer={currentBuyer} />
+        </Route>
+        <Route exact path="/myProfile">
+          <BuyerProfile
+            currentBuyer={currentBuyer}
+            setCurrentBuyer={setCurrentBuyer}
+          />
+        </Route>
+        <Route path="/artworks/:id">
+          <ArtworkDetails currentBuyer={currentBuyer} />
+        </Route>
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client_side/src/components/HomePage.jsx
+++ client_side/src/components/HomePage.jsx
@@ -1,100 +1,114 @@
-import React, { useState, useEffect} from "react"
-import { useHistory } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Artworks from "./Artworks";
 import BuyerProfile from "./BuyerProfile";
 
-function HomePage({ onLoginClick, setCurrentBuyer, currentBuyer, onChangeBuyers, allBuyers, setAllBuyers }) {
-    const [buyerName, setBuyername] = useState("");
-    // const [allBuyers, setAllBuyers] = useState([]);
-    // const [isLoggedIn, setIsLoggedIn] = useState(false);
+function HomePage({
+  onLoginClick,
+  setCurrentBuyer,
+  isLoggedIn,
+  currentBuyer,
+  onChangeBuyers,
+  allBuyers,
+  setAllBuyers,
+}) {
+  const [buyerName, setBuyername] = useState("");
+  // const [allBuyers, setAllBuyers] = useState([]);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-// POSTing new buyer profile
-    function createBuyer(e) {
-        e.preventDefault();
+  // POSTing new buyer profile
+  function createBuyer(e) {
+    e.preventDefault();
 
-        const newBuyer = {
-            first_name: buyerName,
-        };
+    const newBuyer = {
+      first_name: buyerName,
+    };
 
-        fetch("http://localhost:9292/buyers", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newBuyer),
-        })
-            .then((res) => res.json())
-            .then((data) => setCurrentBuyer(data));
+    fetch("http://localhost:9292/buyers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBuyer),
+    })
+      .then((res) => res.json())
+      .then((data) => setCurrentBuyer(data));
 
-        setBuyername("");
-        alert("New buyer has been created!");
-    }
+    setBuyername("");
+    alert("New buyer has been created!");
+  }
 
-// // GETing buyer profiles
-//     useEffect(() => {
-//         fetch("http://localhost:9292/buyers")
-//         .then((res) => res.json())
-//         .then(setAllBuyers);
-//     }, []);
+  // // GETing buyer profiles
+  //     useEffect(() => {
+  //         fetch("http://localhost:9292/buyers")
+  //         .then((res) => res.json())
+  //         .then(setAllBuyers);
+  //     }, []);
 
-// // finding current buyer
-//     function handleChangeBuyers(e){
-//         // let fullName = `${buyer.first_name} + ${buyer.last_name}`
-//         let foundBuyer = allBuyers.find((buyer) => buyer.first_name === e.target.value);
-//         setCurrentBuyer(foundBuyer);
-//     }
+  // // finding current buyer
+  //     function handleChangeBuyers(e){
+  //         // let fullName = `${buyer.first_name} + ${buyer.last_name}`
+  //         let foundBuyer = allBuyers.find((buyer) => buyer.first_name === e.target.value);
+  //         setCurrentBuyer(foundBuyer);
+  //     }
 
-
-    return (
-        <div>
-            <div>
-                <div className="about">
-                    <h1>Welcome to <span className="title">Rotheby's</span></h1>
-                </div>
-            <div className="returningBuyer">
-                <h3> Returning Buyer? </h3>
-                <h4> Find your Name </h4>
-                <div className="selectName">
-                    <select onChange={(e) => onChangeBuyers(e)}>
-                        <option></option>
-                            {allBuyers.map((buyer) => (
-                                <option key={buyer.id}>{buyer.first_name}</option>
-                            ))}
-                    </select>
-                    <div className="selectArrow"></div>
-                </div>
-            </div>
-            <div className="newBuyer">
-                <h3> New Buyer? </h3>
-                <h4> Enter Your Name </h4>
-                <form onSubmit={(e) => createBuyer(e)}>
-                    <input
-                        value={buyerName}
-                        onChange={(e) => setBuyername(e.target.value)}
-                        type = "text"
-                        placeholder="Enter your name..."
-                    >
-                    </input>
-                    <br></br>
-                    <button
-                        className="submitNewNameBtn"
-                        type ="sumbit"
-                        // onClick={handleLoginClick}
-                    >
-                        Create Buyer
-                    </button>
-                </form>
-                <br></br>
-                <br></br>
-            </div>
-            <div className="Submit Profile">
-                <button className="glow-on-hover" onClick={onLoginClick}>
-                    Login
-                </button>
-            </div>
-            </div>
+  return (
+    <div>
+      <div>
+        <div className="about">
+          <h1>
+            Welcome to <span className="title">Rotheby's</span>
+          </h1>
         </div>
-    );
+        <div className="returningBuyer">
+          <h3> Returning Buyer? </h3>
+          <h4> Find your Name </h4>
+          <div className="selectName">
+            <select onChange={(e) => onChangeBuyers(e)}>
+              <option></option>
+              {allBuyers.map((buyer) => (
+                <option key={buyer.id}>{buyer.first_name}</option>
+              ))}
+            </select>
+            <div className="selectArrow"></div>
+          </div>
+        </div>
+        <div className="newBuyer">
+          <h3> New Buyer? </h3>
+          <h4> Enter Your Name </h4>
+          <form onSubmit={(e) => createBuyer(e)}>
+            <input
+              value={buyerName}
+              onChange={(e) => setBuyername(e.target.value)}
+              type="text"
+              placeholder="Enter your name..."
+            ></input>
+            <br></br>
+            <button
+              className="submitNewNameBtn"
+              type="sumbit"
+              // onClick={handleLoginClick}
+            >
+              Create Buyer
+            </button>
+          </form>
+          <br></br>
+          <br></br>
+        </div>
+        <div className="Submit Profile">
+          {isLoggedIn ? (
+            <button className="glow-on-hover" onClick={onLoginClick}>
+              Logout
+            </button>
+          ) : (
+            <button className="glow-on-hover" onClick={onLoginClick}>
+              Login
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
